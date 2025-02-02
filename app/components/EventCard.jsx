36b70072788afc9adce2ba721a675fc4f2b2de97
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-02-03 00:08:27 +0530

    Add Event Cards

--- app/components/EventCard.jsx
+++ app/components/EventCard.jsx
@@ -10,7 +10,7 @@ export default function EventCard({
     const isEven = parseInt(number) % 2 === 0;
 
     return (
-        <div className={`flex px-16 container ${isEven ? 'justify-end' : 'justify-start'}`}>
+        <div className={`flex flex-col-reverse items-end md:items-center  md:flex-row px-2 sm:px-16 container ${isEven ? 'md:justify-end' : 'justify-start'}`}>
         <div className="flex flex-col items-center justify-center">
             <h2 className='w-48 text-lg md:text-2xl text-end font-semibold bg-gradient-to-r from-[#00C2FF] to-[#00C187] bg-clip-text text-transparent'>
                 {title}
@@ -18,7 +18,7 @@ export default function EventCard({
             <p className='w-48 text-sm md:text-lg text-white text-wrap text-end'>{description}</p>
         </div>
         <div className="group relative">
-            <img src={imageSrc} alt="Logic Lounge" className='bg-contain' />
+            <img src={imageSrc} alt="Logic Lounge" className='bg-contain min-w-[300px]' />
             <div className="absolute top-0 right-0 flex items-center justify-center">
                 <img src="/ellipse.png" alt="circle" className="relative" />
                 <span className="absolute text-white font-bold">{number}</span>
